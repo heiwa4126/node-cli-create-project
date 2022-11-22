7fb30f83d1e524e8f882e05394e572e6bba42b4b
heiwa4126 <heiwa4126@gmail.com>
2022-11-22 15:51:03 +0900

    update

---
+++ bin/create-project.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// -*- mode: JavaScript -*-
+
+// require = require('esm')(module /*, オプション*/);
+// require('../src/cli').cli(process.argv);
+
+import {cli} from '../src/cli.js';
+cli(process.argv);
