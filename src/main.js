58c527bbbe7fe3d8fcddb524310e2fc01d8d482a
heiwa4126 <heiwa4126@gmail.com>
2022-11-22 16:19:43 +0900

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import chalk from 'chalk';
 import fs from 'fs';
 import ncp from 'ncp';
 import path from 'path';
-import { promisify } from 'util';
+import {promisify} from 'util';
+import {execa} from 'execa';
+import Listr from 'listr';
+import {projectInstall} from 'pkg-install';
+
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
@@ -11,6 +15,16 @@ async function copyTemplateFiles(options) {
   return copy(options.templateDirectory, options.targetDirectory, {
     clobber: false,
   });
+}
+
+async function initGit(options) {
+  const result = await execa('git', ['init'], {
+    cwd: options.targetDirectory,
+  });
+  if (result.failed) {
+    return Promise.reject(new Error('Failed to initialize git'));
+  }
+  return;
 }
 
 export async function createProject(options) {
@@ -34,9 +48,30 @@ export async function createProject(options) {
     process.exit(1);
   }
 
-  console.log('Copy project files');
-  await copyTemplateFiles(options);
+  const tasks = new Listr([
+    {
+      title: 'プロジェクトファイルのコピー',
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: 'gitの初期化',
+      task: () => initGit(options),
+      enabled: () => options.git,
+    },
+    {
+      title: '依存関係のインストール',
+      task: () =>
+      projectInstall({
+        cwd: options.targetDirectory,
+      }),
+      skip: () =>
+      !options.runInstall
+        ? '依存関係を自動的にインストールする場合は --install オプションを渡してください'
+        : undefined,
+    },
+  ]);
 
-  console.log('%s Project ready', chalk.green.bold('DONE'));
+  await tasks.run();
+  console.log('%s プロジェクトの準備ができました', chalk.green.bold('完了'));
   return true;
 }
